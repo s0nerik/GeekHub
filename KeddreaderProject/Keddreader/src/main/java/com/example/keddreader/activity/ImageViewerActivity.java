0b1bcdbe1d1bafa128ccd4f4ed0f837747697016
s0nerik <sonerik@mail.ua>
2013-11-17 15:26:30 +0200

    Added ProgressBar to image viewer.

--- KeddreaderProject/Keddreader/src/main/java/com/example/keddreader/activity/ImageViewerActivity.java
+++ KeddreaderProject/Keddreader/src/main/java/com/example/keddreader/activity/ImageViewerActivity.java
@@ -1,12 +1,17 @@
 package com.example.keddreader.activity;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.ProgressBar;
 
 import com.example.keddreader.R;
 import com.example.keddreader.custom_view.TouchImageView;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.assist.ImageLoadingListener;
 
 public class ImageViewerActivity extends ActionBarActivity {
 
@@ -21,9 +26,31 @@ public class ImageViewerActivity extends ActionBarActivity {
         url = getIntent().getStringExtra("url");
 
         TouchImageView imageView = (TouchImageView) findViewById(R.id.image_viewer);
+
+        final ProgressBar spinner = (ProgressBar) findViewById(R.id.image_viewer_spinner);
+
         ImageLoader imageLoader = ImageLoader.getInstance();
         imageLoader.init(ImageLoaderConfiguration.createDefault(this));
-        imageLoader.displayImage(url, imageView);
+        imageLoader.displayImage(url, imageView,  new ImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+                spinner.setVisibility(View.VISIBLE);
+            }
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                spinner.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onLoadingComplete(String s, View view, Bitmap bitmap) {
+                spinner.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onLoadingCancelled(String s, View view) {
+                spinner.setVisibility(View.GONE);
+            }
+        });
     }
 
 }
